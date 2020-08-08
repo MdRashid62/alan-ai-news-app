c25b59ec18fa3b90bd8ba7e22566bfc164dd9f61
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-08-08 13:38:54 +0500

    news card ui

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,53 +1,44 @@
-import React from "react";
-import {
-  Card,
-  CardActions,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Button,
-  Typography,
-} from "@material-ui/core";
+import React, { useState, useEffect, createRef } from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import classNames from 'classnames';
 
-const NewsCard = ({
-  article: { description, publishedAt, source, title, url, urlToImage },
-  i,
-}) => {
+import useStyles from './styles';
+
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, activeArticle, i }) => {
+  const classes = useStyles();
+  const [elRefs, setElRefs] = useState([]);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+  useEffect(() => {
+    window.scroll(0, 0);
+
+    setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+  }, []);
+
+  useEffect(() => {
+    if (i === activeArticle && elRefs[activeArticle]) {
+      scrollToRef(elRefs[activeArticle]);
+    }
+  }, [i, activeArticle, elRefs]);
+
   return (
-    <card>
-      <CardActionArea>
-        <CardMedia
-          image={
-            urlToImage ||
-            "https://www.bing.com/images/search?view=detailV2&ccid=OMs0%2bG41&id=3F0807B3F60DEB2A3B7DF23574BD9B0614B2A576&thid=OIP.OMs0-G41pSvQR2ndH6EZNwHaFg&mediaurl=http%3a%2f%2f2.bp.blogspot.com%2f-0k2zBsKQNcQ%2fTaYmaM6kfZI%2fAAAAAAAAAA0%2ffDH8FP8jSQw%2fs1600%2fnews.jpg&exph=1000&expw=1344&q=news+images&simid=608032696154786066&ck=48A882FE09718EBF81B0B75E9644E01A&selectedIndex=0&FORM=IRPRST&ajaxhist=0"
-          }
-        />
-        <div>
-          <Typography variant="body2" color="textSecondary" component="h2">
-            {new Date(publishedAt).toDateString()}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="h2">
-            {source.name}
-          </Typography>
+    <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia className={classes.media} image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} title={title} />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
         </div>
-        <Typography gutterBottom variant="h5">
-          {title}
-        </Typography>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{title}</Typography>
         <CardContent>
-          <Typography variant="body2" color="textPrimary" component="p">
-            {description}
-          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Learn more
-        </Button>
-        <Typography variant="h5" color="textSecondary">
-          {i + 1}
-        </Typography>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" href={url}>Learn More</Button>
+        <Typography variant="h5" color="textSecondary" component="h2">{i + 1}</Typography>
       </CardActions>
-    </card>
+    </Card>
   );
 };
 
