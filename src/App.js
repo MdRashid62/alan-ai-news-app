ba7c7caa0c1ed670d1b21450b6ca8706770d37f9
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-08-08 15:01:17 +0500

    more functionalities added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import wordsToNumbers from 'words-to-numbers';
+
 import NewsCards from './components/NewsCards/NewsCards';
 import useStyles from './styles.js';
 
@@ -8,14 +10,22 @@ const alanKey = '35fd9cf6f5fce9955b666f2ba446f4c32e956eca572e1d8b807a3e2338fdd0d
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
     const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if(command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1 );
+                } else if(command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+
+                    window.open(articles[number].url, '_blank');
                 }
             }
         })
@@ -26,7 +36,7 @@ const App = () => {
             <div className={classes.logoContainer}>
                 <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
             </div>
-            <NewsCards articles={newsArticles}/>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
         </div>
     )
 }
