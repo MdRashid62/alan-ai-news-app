08af6dba9070b2181b2314631e0d729e177b17ee
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-08-08 14:03:25 +0500

    home page visual

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
 const alanKey = '35fd9cf6f5fce9955b666f2ba446f4c32e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -21,7 +23,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan AI News App</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+            </div>
             <NewsCards articles={newsArticles}/>
         </div>
     )
